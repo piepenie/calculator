b4b0e4364e86926b7a0debb45cad0d0d2a2be44c
piepenie <piepenie@daum.net>
2026-06-29 15:52:05 +0900

    Loop Calc Success

--- calculator/src/main/java/Main.java
+++ calculator/src/main/java/Main.java
@@ -1,44 +1,66 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("---계산기 프로그램 실행중---");
+        // 해당 results 는 결과 값들을 보관하는 창고.
+        List<Double> results = new ArrayList<>();
 
-        System.out.println("연산자를 입력해주세요 (+, -, *, /) : ");
-        String operator = sc.next();
 
-        System.out.println("a 값을 입력해주세요 : ");
-        int a = sc.nextInt();
-        System.out.println("b 값을 입력해주세요 : ");
-        int b = sc.nextInt();
+        while(true){
+            System.out.println("---계산기 프로그램 실행중---");
 
-        switch (operator){
-            case "+":
-                System.out.println("a+b의 값 : " + (a + b));
-                break;
+            // 값을 입력하는게 더 먼저 나와야 논리전개에 안정적임
+            System.out.println("a 값을 입력해주세요 : ");
+            double a = sc.nextDouble();
+            System.out.println("b 값을 입력해주세요 : ");
+            double b = sc.nextDouble();
 
-            case "-":
-                System.out.println("a-b의 값 : " + (a - b));
-                break;
+            System.out.println("연산자를 입력해주세요 (+, -, *, /) : ");
+            String operator = sc.next();
 
-            case "*":
-                System.out.println("a*b의 값 : " + (a * b));
-                break;
 
-            case "/":
-                if (b != 0 ){
-                    System.out.println("a/b의 값 : " + (a / b));
-                } else {
-                    System.out.println("0으로는 나눌 수 없습니다.");
-                }
-                break;
-            default:
-                System.out.println("연산자를 다시 확인해주세요.");
-                break;
+            // 해당 result 는 연산 단일 결과값 그자체.
+            double result = 0.0;
+            // boolean
+            boolean isSuccess = true;
+
+
+            switch (operator){
+                case "+":
+                    result = a + b;
+                    break;
+
+                case "-":
+                    result = a - b;
+                    break;
+
+                case "*":
+                    result = a * b;
+                    break;
+
+                case "/":
+                    if (b != 0.0 ){
+                        result = a / b;
+                    } else {
+                        System.out.println("0으로는 나눌 수 없습니다.");
+                        isSuccess = false;
+                    }
+                    break;
+                default:
+                    System.out.println("연산자를 다시 확인해주세요.");
+                    isSuccess = false;
+                    break;
+            }
+
+            if(isSuccess){
+                System.out.println("결과 : " + result);
+                results.add(result);
+            }
         }
 
-        sc.close();
     }
 }
